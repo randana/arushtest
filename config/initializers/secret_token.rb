5ea6865527f8c3a1c17f7719397957372fcfcac1
arush dixit <dixit.arush@gmail.com>
2014-10-04 15:46:43 +0530

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Arushtest::Application.config.secret_key_base = 'bb79c2530ec370225a104068bcb59f077c39f682e26a1ab98b067ee6395d3bee0d747e68da4fddf3530a12151ec874666ff65d9d3c1a2c87d034fb7e52d3ebe9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Arushtest::Application.config.session_store :cookie_store, key: '_arushtest_session'
